a1d118ce92a3c96896a1319b21e14d3eee0997fb
aomond <antoine.omond@imt-atlantique.fr>
2023-10-04 17:03:01 +0200

    Remove individuals files after merge

--- merge_result_files.py
+++ merge_result_files.py
@@ -11,9 +11,12 @@ merged_results = {}
 print("Merging files...")
 for file_name in os.listdir(results_dir_to_merge):
     node_num = int(Path(file_name).stem)
-    with open(f"{results_dir_to_merge}/{file_name}") as f:
+    file_path = f"{results_dir_to_merge}/{file_name}"
+    with open(file_path) as f:
         merged_results[node_num] = yaml.safe_load(f)
+    os.remove(file_path)
 
+os.rmdir(results_dir_to_merge)
 target_file_name = f"./{expe_name}.yaml"
 print(f"Dump results at {target_file_name}")
 with open(target_file_name, "w") as f:
