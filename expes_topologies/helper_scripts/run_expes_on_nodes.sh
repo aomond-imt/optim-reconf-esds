edd391d254aa2996bd30e392c9a5eae342579195
aomond <antoine.omond@imt-atlantique.fr>
2023-11-19 10:57:23 +0100

    Fixes

--- expes_topologies/helper_scripts/run_expes_on_nodes.sh
+++ expes_topologies/helper_scripts/run_expes_on_nodes.sh
@@ -2,9 +2,9 @@
 
 hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
 for host in $hosts; do
-  host_split=($(echo "$host" | tr ";" " "))
-  host_node=${host_split}[0]
+  host_split=($(echo "$host" | tr "." " "))
+  host_node=${host_split[0]}
   echo "starting on host $host_node"
   tmux kill-session -t "$host_node"
-  tmux new-session -d -s "$host_node" "ssh $host_node; so; python3 run_experiments.py"
+  tmux new-session -d -s "$host_node" "ssh $host_node 'cd ~/optim-reconf-esds; source venv/bin/activate; cd expes_topologies; export PYTHONPATH=$PYTHONPATH:$(pwd); python3 run_experiments.py; sleep infinity'"
 done
--- expes_topologies/helper_scripts/stop_expes_on_nodes.sh
+++ expes_topologies/helper_scripts/stop_expes_on_nodes.sh
@@ -2,8 +2,8 @@
 
 hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
 for host in $hosts; do
-  host_split=($(echo "$host" | tr ";" " "))
-  host_node=${host_split}[0]
+  host_split=($(echo "$host" | tr "." " "))
+  host_node=${host_split[0]}
   echo "killing tmux session on host $host_node"
   tmux kill-session -t "$host_node"
   ssh "$host_node" "kill $(ps -aux | pgrep -f esds)"
