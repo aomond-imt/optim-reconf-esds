d5766a8e7fef49489ff548450f71b6c66c208c49
aomond <antoine.omond@imt-atlantique.fr>
2023-11-19 10:18:26 +0100

    Fixed helper scripts

--- helper_scripts/stop_expes_on_nodes.sh
+++ helper_scripts/stop_expes_on_nodes.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-#set -e
-
-## declare an array variable
-declare -a hosts=($(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]'))
-
-#declare -a hosts=(
-#  "gros-21"
-#  "gros-22"
-#  "gros-23"
-#  "gros-24"
-#  "gros-25"
-#)
-
-# get length of an array
-hosts_length=${#hosts[@]}
-
-# use for loop to read all values and indexes
-for (( i=0; i<${hosts_length}; i++ ));
-do
-  echo "killing tmux session on host ${hosts[$i]}"
-  ssh ${hosts[$i]} "tmux kill-session -t $i"
-  ssh ${hosts[$i]} "kill $(ps -aux | pgrep -f esds)"
+hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
+for host in $hosts; do
+  echo "killing tmux session on host $host"
+  tmux kill-session -t "$host"
+  ssh "$host" "kill $(ps -aux | pgrep -f esds)"
 done
