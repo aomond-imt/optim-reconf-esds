48da0b29e1c0fa8f456a7dd01edbb61cd02e1b4f
aomond <antoine.omond@imt-atlantique.fr>
2023-11-19 10:48:44 +0100

    Fix helper script

--- expes_topologies/helper_scripts/run_expes_on_nodes.sh
+++ expes_topologies/helper_scripts/run_expes_on_nodes.sh
@@ -2,7 +2,9 @@
 
 hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
 for host in $hosts; do
-  echo "starting on host $host"
-  tmux kill-session -t "$host"
-  tmux new-session -d -s "$host" "ssh $host; so; python3 run_experiments.py"
+  host_split=($(echo "$host" | tr ";" " "))
+  host_node=${host_split}[0]
+  echo "starting on host $host_node"
+  tmux kill-session -t "$host_node"
+  tmux new-session -d -s "$host_node" "ssh $host_node; so; python3 run_experiments.py"
 done
--- expes_topologies/helper_scripts/stop_expes_on_nodes.sh
+++ expes_topologies/helper_scripts/stop_expes_on_nodes.sh
@@ -2,7 +2,9 @@
 
 hosts=$(echo $(oarstat -fu --json) | jq -r '.[].assigned_network_address[]')
 for host in $hosts; do
-  echo "killing tmux session on host $host"
-  tmux kill-session -t "$host"
-  ssh "$host" "kill $(ps -aux | pgrep -f esds)"
+  host_split=($(echo "$host" | tr ";" " "))
+  host_node=${host_split}[0]
+  echo "killing tmux session on host $host_node"
+  tmux kill-session -t "$host_node"
+  ssh "$host_node" "kill $(ps -aux | pgrep -f esds)"
 done
